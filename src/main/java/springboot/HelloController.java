81cf99ea0747f9d5bf4513aff809709c57fdf8ba
ya_semin <esemin83@gmail.com>
2020-02-17 00:04:10 +0300

    springboot example

---
+++ src/main/java/springboot/HelloController.java
@@ -0,0 +1,20 @@
+package springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/get")
+    public String getMethod() {
+        return "get";
+    }
+}
