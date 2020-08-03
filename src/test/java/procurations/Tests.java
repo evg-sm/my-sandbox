c72d50c97fefc65ced30873272aa60e669f38f2a
esemin83 <esemin83@gmail.com>
2020-08-03 13:41:08 +0300

    # This is a combination of 2 commits.

--- src/test/java/procurations/Tests.java
+++ src/test/java/procurations/Tests.java
@@ -14,5 +14,24 @@ public class Tests {
         System.out.println("\n");
     }
 
+    @Test
+    public void test02() {
+        System.out.println("\n");
+
+        ProcurationsActions actions = new AccountProcuration();
+        System.out.println("procurations is " + actions.addPrincipalClient().addAtterneyClient().create());
+
+        System.out.println("\n");
+    }
+
+    @Test
+    public void test03() {
+        System.out.println("\n");
+
+        ProcurationsActions actions = new AccountProcuration();
+        System.out.println("procurations is " + actions.addPrincipalClient().addAtterneyClient().create());
+
+        System.out.println("\n");
+    }
 
 }
